269770389ab65893d731cc3d28d3b15876ec2903
mjh131 <72174676+mjh131@users.noreply.github.com>
2021-05-28 12:10:12 -0400

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,1 +1,12 @@
 // validation script here
+const input = document.querySelectorAll('input');
+
+const patterns = {
+  telephone: /^\d[11]$/
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    console.log(e.target.attributes.name.value);
+  });
+});
