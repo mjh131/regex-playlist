a1b3f626cfb3c9eb94e787704b589ae23f856e75
mjh131 <72174676+mjh131@users.noreply.github.com>
2021-05-30 09:05:16 -0400

    Update validation.js

--- validation.js
+++ validation.js
@@ -5,8 +5,18 @@ const patterns = {
   telephone: /^\d[11]$/
 };
 
+// validation function
+function validate(field,regex) {
+    if(regex.test(field.value)){
+      field.className = 'valid';
+      } else {
+        field.className = 'invlaid';
+    }
+}
+
 inputs.forEach((input) => {
   input.addEventListener('keyup', (e) => {
-    console.log(e.target.attributes.name.value);
+    //console.log(e.target.attributes.name.value);
+    validate(e.target, patterns['telephone'])
   });
 });
